95fe92600b2d689e42b0776939fb2b2a88bb670b
Myrl Kressara Orkut <MyrlKressara@gmail.com>
2015-10-13 11:14:46 -0400

    What. The. Fuck.

--- MyHTTP.hs
+++ MyHTTP.hs
@@ -1,22 +1,72 @@
-module MyHTTP (contentLength, contentType, headerEnd, hContentType, hHeaderEnd, hContentLength) where
+{-# LANGUAGE OverloadedStrings #-}
+module MyHTTP (Request(Get), Connection(KeepAlive, Close), Response(connection, contentType), sRespond, okResponse, notImplementedResponse, notFoundResponse, sResponseCode, sContentLength, sContentType) where
 
-import System.IO
-import Text.Printf
+import qualified Data.ByteString as B hiding (pack)
+import qualified Data.ByteString.Char8 as B (pack)
+import Network.Socket hiding (send, sendTo, recv, recvFrom)
+import Network.Socket.ByteString
+import Data.Maybe
 
-contentType :: String -> IO ()
-contentType = putStr . printf "\r\nContent-type: %s"
+data Request =
+    Get (Maybe B.ByteString) (Maybe B.ByteString) (Int, Int) -- Path, Version
+    deriving (Show)
 
-contentLength :: Int -> IO ()
-contentLength = putStr . printf "\r\ncontent-length: %d"
+data Response = Response
+    { responseCode  :: Int
+    , connection    :: Connection
+    , contentType   :: Maybe B.ByteString
+    }
 
-headerEnd :: IO ()
-headerEnd = putStr "\r\n\r\n"
+data Connection = KeepAlive Int | Close
 
-hContentType :: Handle -> String -> IO ()
-hContentType h = hPutStr h . printf "\r\nContent-type: %s"
+godResponse :: Response
+godResponse = Response { responseCode  = 0
+                       , connection    = MyHTTP.KeepAlive 15
+                       , contentType   = Nothing 
+                       }
 
-hContentLength :: Handle -> Int -> IO ()
-hContentLength h = hPutStr h .printf "\r\ncontent-length: %d"
+notImplementedResponse :: Response
+notImplementedResponse = godResponse { responseCode = 501 }
 
-hHeaderEnd :: Handle -> IO ()
-hHeaderEnd h = hPutStr h "\r\n\r\n"
+okResponse :: Response
+okResponse = godResponse{ responseCode = 200 }
+
+notFoundResponse :: Response
+notFoundResponse = godResponse{ responseCode = 404 }
+
+-- hDoMaybe :: (a -> IO ()) -> Maybe a -> IO ()
+-- hDoMaybe = maybe (return ())
+
+sRespond :: Socket -> Response -> Maybe B.ByteString -> IO ()
+sRespond s r x = do
+    sendMany s foo
+    where
+        foo = 
+          [ sResponseCode  $ responseCode r
+          , sConnection    $ connection  r
+          ] ++ (catMaybes
+          [ sContentType   <$> contentType r
+          , sContentLength <$> x
+          , Just "\r\n\r\n"
+          , x
+          ])
+    --sendAll s . sResponseCode $ responseCode r
+    --sendAll s . sConnection $ connection r
+    --hDoMaybe (sendAll s . sContentType)   $ contentType   r
+    --hDoMaybe (sendAll s . sContentLength) $ contentLength r
+    --sendAll s  "\r\n\r\n"
+      
+sResponseCode :: Int -> B.ByteString
+sResponseCode 200 = "HTTP/1.1 200 OK"
+sResponseCode 404 = "HTTP/1.1 404 Not Found"
+sResponseCode _   = "HTTP/1.1 501 Not Implemented"
+
+sContentType :: B.ByteString -> B.ByteString
+sContentType = B.append "\r\nContent-type: "
+
+sContentLength :: B.ByteString -> B.ByteString
+sContentLength = B.append "\r\ncontent-length: " . B.pack . show . B.length
+
+sConnection :: Connection -> B.ByteString
+sConnection Close                = "\r\nConnection: Close"
+sConnection (MyHTTP.KeepAlive x) = B.append "\r\nConnection: keep-alive\r\nTimeout: " (B.pack $ show x)
--- main2.hs
+++ main2.hs
@@ -1,57 +1,109 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Prelude hiding (takeWhile)
+import System.Random
 import Control.Monad
-import Data.Char
-import Network
-import System.IO
-import System.Directory
-import Data.Bool
-import Data.List
-import System.FilePath
-import Data.Function
+import Network.Socket hiding (send, sendTo, recv, recvFrom)
+import Network.Socket.ByteString
+import Data.Attoparsec.ByteString.Char8
+--import qualified Data.IntMap as IntMap
 import qualified Control.Exception as E
-import Text.Parsec
+import qualified Data.ByteString.Char8 as B
 import qualified Data.Foldable as F
 
-data Request =
-    Get (Maybe String) (Int, Int) -- Path, Version
-    deriving (Show)
+import MyHTTP
 
-blank = satisfy go <?> "blank"
+homeDir :: B.ByteString
+homeDir = "/home/myrl/Website/files"
+
+blanks :: Parser ()
+blanks = pure () <* takeWhile go <?> "blanks"
     where
+        go '\r' = False
         go '\n' = False
         go x    = isSpace x
 
-blanks = skipMany blank
-keyword s = try (string s) *> blanks
+keyword :: B.ByteString -> Parser ()
+keyword s = string s *> blanks <?> "keyword"
 
-headerGetParser = Get <$ keyword "GET" <*> path <*> version
+headerGetParser :: Parser Request
+headerGetParser = Get <$ keyword "GET" <*> path <*> args <*> version <* flushGet <?> "headerGetParser"
     where
-        path    = shrinkPath <$ char '/' <*> sepBy (many $ noneOf " /") (char '/') <* blanks
-        version = ((,) `on` read) <$ keyword "HTTP/" <*> many1 digit <* char '.' <*> many1 digit
+        path     = shrinkPath <$ slash <*> sepBy fileName slash <* blanks
+        fileName = takeWhile1 $ notInClass " /?"
+        slash    = char '/'
+        args     = option Nothing $ Just <$ char '?' <*> takeTill (== ' ') <* blanks
+        version  = (,) <$ keyword "HTTP/" <*> decimal <* satisfy (inClass ".") <*> decimal <?> "version"
+        
+        flushGet = (manyTill anyChar $ string "\r\n\r\n") <?> "flushGet"
 
-headerParser :: Parsec String () Request
+headerParser :: Parser Request
 headerParser = try headerGetParser
 
-shrinkPath :: [String] -> Maybe String
-shrinkPath = fmap (('/':) =<<) . F.foldrM body_ []
+shrinkPath :: [B.ByteString] -> Maybe B.ByteString
+shrinkPath = fmap (B.append homeDir . B.concat . fmap (B.cons '/')) . F.foldrM body_ []
   where
       body_ ".." []     = Nothing
-      body_ ".." (x:xs) = Just xs
+      body_ ".." (_:xs) = Just xs
       body_ "."  xs     = Just xs
       body_ ""   xs     = Just xs
       body_ x    xs     = Just (x:xs)
 
-main = withSocketsDo $ do
-    sock <- listenOn $ PortNumber 80
-    forever $ do
-        (h, _, _) <- accept sock
-        req <- E.try $ hGetLine h
-        case req of
-          Left  (e :: E.IOException) ->
-              print $ "IO Error encountered: " ++ show e
-          Right req                  ->
-              asdas
-    
+takeExt :: Parser B.ByteString
+takeExt = last <$> takeWhile1 (/= '.') `sepBy` char '.' 
 
+--takeExt :: Parser B.ByteString
+--takeExt = takeTill (== '.') *> char '.' *> takeByteString
+    
+handleRequest :: Socket -> B.ByteString -> Int -> IO ()
+handleRequest s r x =
+--    parseWith (recv s 1024 >>= (\x -> print (B.append "Recv: " x) >> return x)) (match headerParser) r >>= \case
+--      (Fail _ es e) -> return () --error $ e ++ (concatMap (" <- " ++) $ es)
+--      (Done r' (str, Get (Just path) _ _))  -> do
+--          print $ B.append "Consumed: " str
+--          print $ "id: " ++ show x
+--          print $ B.append "r: " r'
+--          let (Right ext) = parseOnly takeExt path
+--          fileHandler ext path
+--          handleRequest s r' x
+    go $ parse headerParser r
+    where
+        go (Fail _ es e) = error $ e ++ concatMap (" <- " ++ ) es
+        go (Partial   f) = recv s 1024 >>= go' f
+        go (Done r' (Get (Just path) _ _)) = do
+            let (Right ext) = parseOnly takeExt path
+            fileHandler ext path
+            handleRequest s r' x
+        go' f str
+            | B.length str == 0 = print ("Connection closed." :: B.ByteString)
+            | otherwise        = go (f str)
+        fileHandler "png"  = fileHandler' "image/png"
+        fileHandler "jpeg" = fileHandler' "image/jpeg"
+        fileHandler "jpg"  = fileHandler' "image/jpeg"
+        fileHandler "js"   = fileHandler' "application/javascript"
+        fileHandler "html" = fileHandler' "text/html"
+        fileHandler "css"  = fileHandler' "text/css"
+        fileHandler "ttf"  = fileHandler' "application-font-sfnt"
+        fileHandler "woff" = fileHandler' "application/font-woff"
+        fileHandler _       = niHandler
+        fileHandler' cType path = do
+            print path
+            file <- B.readFile . B.unpack $ path
+            sRespond s (okResponse { --connection    = Close
+                                    contentType   = Just cType
+                                   }) $ Just file
+        niHandler _ = sRespond s (notFoundResponse)  Nothing
+
+main :: IO ()
+main = withSocketsDo $ do
+    sock <- socket AF_INET Stream 0
+    setSocketOption sock ReusePort 1
+    bind sock (SockAddrInet 8080 iNADDR_ANY)
+    listen sock 5
+    forever $ do
+        E.bracketOnError (accept sock)
+                         (\(s, _) -> close s)
+                         (\(s, _) -> (randomIO :: IO Int) >>= handleRequest s "")
